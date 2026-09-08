93084dc44ee3692f45b889c154129c37ec57d060
Shredthegnar1980 <callenhpriest@gmail.com>
2026-09-08 18:22:25 +0000

    my first commit

---
+++ crash-course.py
@@ -0,0 +1,35 @@
+#comment
+
+# here
+# is 
+# a
+# comment
+
+print("hello world!")
+
+#Variable declerations and data types:
+
+a = 4 #integer
+b = 5.5  #float
+c = "CSAEA"  #string
+d = False   #boolean
+
+print(a, b, c, d)
+
+# OPERATORS
+# + - / *  % ** //
+# += -= /=
+
+e = 7 + 2
+print(e)
+e += 7
+print(e)
+
+# f-string
+
+print(f"e is equal to {e}")
+
+e -= 2
+e+= 7
+
+print(f"e is NOW equal to {e}")
