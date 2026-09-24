95136f51469ae38bda7c5ac08e49e18ba1b9fe87
Shredthegnar1980 <callenhpriest@gmail.com>
2026-09-24 17:13:37 +0000

    challenges

---
+++ crash_course_challenges.py
@@ -0,0 +1,18 @@
+# question 4
+q = 13
+if q >= 90:
+    print("A")
+elif q >= 80:
+    print("B")
+elif q>= 70:
+    print("C")
+elif q>= 60:
+    print("D")
+else:
+    print("F")
+
+
+# question 5
+password = 12345
+attempt = 12345
+if password = attempt:
